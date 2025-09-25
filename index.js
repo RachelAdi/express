84390a601299ebef46d892a7c43b7d1a23b379c6
RachelAdi <r0548571737@gmail.com>
2025-09-25 14:54:24 +0300

    added logger file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const { logger } = require("./logger");
+const bookRouter = require("./router/bookRouter");
+
 const app = express();
-
 app.use(express.json());
+app.use(logger);
 
-const booksRouter = require("./router/bookRouter");
-
-app.use("/books", booksRouter);
+app.use("/books", bookRouter);
 
 const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
